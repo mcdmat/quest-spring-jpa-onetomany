2db00401c9754cb7632733cc77372f3f9a24e6b9
mcdmat <[matheyd@gmail.com](mailto:matheyd@gmail.com)>
2023-03-14 16:22:29 +0100

    solution

--- src/main/java/com/wildcodeschool/wildandwizard/entity/School.java
+++ src/main/java/com/wildcodeschool/wildandwizard/entity/School.java
@@ -1,9 +1,7 @@
 package com.wildcodeschool.wildandwizard.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 public class School {
@@ -14,7 +12,8 @@ public class School {
     private String name;
     private Long capacity;
     private String country;
-
+    @OneToMany(mappedBy = "school", cascade = CascadeType.ALL)
+    private List<Wizard> wizards;
     public School() {
     }
 
@@ -49,4 +48,6 @@ public class School {
     public void setCountry(String country) {
         this.country = country;
     }
+    public List<Wizard> getWizards() { return wizards;}
+    public void setWizards(List<Wizard> wizards) {this.wizards = wizards;}
 }
--- src/main/java/com/wildcodeschool/wildandwizard/entity/Wizard.java
+++ src/main/java/com/wildcodeschool/wildandwizard/entity/Wizard.java
@@ -1,9 +1,6 @@
 package com.wildcodeschool.wildandwizard.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.sql.Date;
 
 @Entity
@@ -18,6 +15,9 @@ public class Wizard {
     private String birthPlace;
     private String biography;
     private boolean muggle;
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.REFRESH, optional = false)
+    @JoinColumn (name = "school_id", nullable = false)
+    private School school;
 
     public Wizard() {
     }
@@ -77,4 +77,7 @@ public class Wizard {
     public void setMuggle(boolean muggle) {
         this.muggle = muggle;
     }
+
+    public School getSchool() { return school;}
+    public void setSchool(School school) { this.school= school;}
 }
